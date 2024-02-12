9135fb0d55d3939b89335aaed979ecbf8ee66605
Andre <andrelrs.v@outlook.com>
2024-02-12 20:47:15 -0300

    feat: adds autor and autordto

---
+++ src/main/java/com/deveficiente/seeddesafiocdc/dto/AutorDTO.java
@@ -0,0 +1,80 @@
+package com.deveficiente.seeddesafiocdc.dto;
+
+import com.deveficiente.seeddesafiocdc.model.Autor;
+import com.deveficiente.seeddesafiocdc.service.EmailUnico;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+public class AutorDTO {
+
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    @Email
+    @NotBlank
+    @EmailUnico
+    private String email;
+
+    @Size(max = 400)
+    @NotBlank
+    private String descricao;
+
+    private LocalDateTime created;
+
+    public AutorDTO() {
+    }
+
+    public AutorDTO(Autor model) {
+        this.nome = model.getNome();
+        this.email = model.getEmail();
+        this.descricao = model.getDescricao();
+        this.created = model.getCreated();
+        this.id = model.getId();
+    }
+
+    public Autor toModel() {
+        var model = new Autor();
+        model.setDescricao(descricao);
+        model.setEmail(email);
+        model.setNome(nome);
+        return model;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+}
---
+++ src/main/java/com/deveficiente/seeddesafiocdc/model/Autor.java
@@ -0,0 +1,65 @@
+package com.deveficiente.seeddesafiocdc.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @Size(max = 400)
+    @NotBlank
+    private String descricao;
+
+    private LocalDateTime created = LocalDateTime.now();
+
+    public Autor() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
