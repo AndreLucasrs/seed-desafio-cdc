0663a66ded386749090dd087f7fc8c025574b2f1
Andre <andrelrs.v@outlook.com>
2024-02-14 00:57:47 -0300

    feat: create livro

---
+++ src/main/java/com/deveficiente/seeddesafiocdc/dto/LivroDTO.java
@@ -0,0 +1,155 @@
+package com.deveficiente.seeddesafiocdc.dto;
+
+import com.deveficiente.seeddesafiocdc.model.Autor;
+import com.deveficiente.seeddesafiocdc.model.Categoria;
+import com.deveficiente.seeddesafiocdc.model.Livro;
+import com.deveficiente.seeddesafiocdc.service.ExistsId;
+import com.deveficiente.seeddesafiocdc.service.UniqueValue;
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.function.Function;
+
+public class LivroDTO {
+
+    private Long id;
+
+    @NotBlank
+    @UniqueValue(fieldName = "titulo", domainClass = Livro.class, message = "Ja existe titulo")
+    private String titulo;
+
+    @NotBlank
+    @Size(max = 500)
+    private String resumo;
+
+    @NotBlank
+    private String sumario;
+
+    @NotNull
+    @Min(value = 20)
+    private BigDecimal preco;
+
+    @NotNull
+    @Min(value = 100)
+    private Integer numeroPaginas;
+
+    @NotBlank
+    @UniqueValue(fieldName = "isbn", domainClass = Livro.class, message = "Ja existe isbn")
+    private String isbn;
+
+    @NotNull
+    @Future
+    private LocalDateTime lancar;
+
+    @ExistsId(fieldName = "id", domainClass = Categoria.class, message = "Nao existe essa categoria")
+    @NotNull
+    private Long categoriaId;
+
+    @ExistsId(fieldName = "id", domainClass = Autor.class, message = "Nao existe esse autor")
+    @NotNull
+    private Long autorId;
+
+    @NotNull
+    public LivroDTO() {
+    }
+
+    public LivroDTO(Livro model) {
+        this.id = model.getId();
+        this.categoriaId = model.getCategoria().getId();
+        this.autorId = model.getAutor().getId();
+        this.titulo = model.getTitulo();
+        this.resumo = model.getResumo();
+        this.sumario = model.getSumario();
+        this.preco = model.getPreco();
+        this.numeroPaginas = model.getNumeroPaginas();
+        this.isbn = model.getIsbn();
+        this.lancar = model.getLancar();
+    }
+
+    public Livro toModel(Function<Long, Autor> carregaAutor,
+                         Function<Long, Categoria> carregaCategoria) {
+
+        Autor autor = carregaAutor.apply(autorId);
+        Categoria categoria = carregaCategoria.apply(categoriaId);
+
+        return new Livro(titulo, resumo, sumario, preco, numeroPaginas, isbn,
+                lancar, categoria, autor);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+
+    public String getSumario() {
+        return sumario;
+    }
+
+    public void setSumario(String sumario) {
+        this.sumario = sumario;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(Integer numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public LocalDateTime getLancar() {
+        return lancar;
+    }
+
+    public void setLancar(LocalDateTime lancar) {
+        this.lancar = lancar;
+    }
+
+    public Long getCategoriaId() {
+        return categoriaId;
+    }
+
+    public void setCategoriaId(Long categoriaId) {
+        this.categoriaId = categoriaId;
+    }
+
+    public Long getAutorId() {
+        return autorId;
+    }
+
+    public void setAutorId(Long autorId) {
+        this.autorId = autorId;
+    }
+}
---
+++ src/main/java/com/deveficiente/seeddesafiocdc/model/Livro.java
@@ -0,0 +1,139 @@
+package com.deveficiente.seeddesafiocdc.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String titulo;
+
+    @NotBlank
+    @Size(max = 500)
+    private String resumo;
+
+    @NotBlank
+    private String sumario;
+
+    @NotNull
+    @Min(value = 20)
+    private BigDecimal preco;
+
+    @NotNull
+    @Min(value = 100)
+    private Integer numeroPaginas;
+
+    @NotBlank
+    @Column(unique = true)
+    private String isbn;
+
+    @NotNull
+    @Future
+    private LocalDateTime lancar;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Categoria categoria;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Autor autor;
+
+    public Livro() {
+    }
+
+    public Livro(String titulo, String resumo, String sumario, BigDecimal preco, Integer numeroPaginas, String isbn, LocalDateTime lancar, Categoria categoria, Autor autor) {
+        this.titulo = titulo;
+        this.resumo = resumo;
+        this.sumario = sumario;
+        this.preco = preco;
+        this.numeroPaginas = numeroPaginas;
+        this.isbn = isbn;
+        this.lancar = lancar;
+        this.categoria = categoria;
+        this.autor = autor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+
+    public String getSumario() {
+        return sumario;
+    }
+
+    public void setSumario(String sumario) {
+        this.sumario = sumario;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(Integer numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public LocalDateTime getLancar() {
+        return lancar;
+    }
+
+    public void setLancar(LocalDateTime lancar) {
+        this.lancar = lancar;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+}
