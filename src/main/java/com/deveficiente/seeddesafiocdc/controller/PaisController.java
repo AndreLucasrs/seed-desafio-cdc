1fe6de7ce6d517dc9e8fb95f5072a64117a36202
Andre <andrelrs.v@outlook.com>
2024-02-19 19:13:17 -0300

    feat: create livro repository and controller

---
+++ src/main/java/com/deveficiente/seeddesafiocdc/controller/PaisController.java
@@ -0,0 +1,27 @@
+package com.deveficiente.seeddesafiocdc.controller;
+
+import com.deveficiente.seeddesafiocdc.dto.PaisDTO;
+import com.deveficiente.seeddesafiocdc.model.Pais;
+import com.deveficiente.seeddesafiocdc.repository.PaisRepository;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pais")
+public class PaisController {
+
+    private final PaisRepository paisRepository;
+
+    public PaisController(PaisRepository paisRepository) {
+        this.paisRepository = paisRepository;
+    }
+
+    @PostMapping
+    public PaisDTO salvar(@Valid @RequestBody PaisDTO dto) {
+        var pais = paisRepository.save(dto.toModel());
+        return new PaisDTO(pais);
+    }
+}
